d17e16a282a590448780b7fdf3d9b1943fc2d18d
Monty <tuckerjsarah@gmail.com>
2019-02-04 18:59:19 -0500

    added user login info

--- assets/main.js
+++ assets/main.js
@@ -9,6 +9,141 @@
   };
   firebase.initializeApp(config);
   var database = firebase.database();
+  
+  function signUpForm(){
+    $('.container').css('display', 'none');
+    $('.signUpForm').css('display', 'block');
+  };
+  function login(){
+    $('.container').css('display', 'none');
+    $('#signInForm').css('display', 'block');
+  };
+  function passResetSetUp(){
+    $('#signInForm').css('display', 'none');
+    $('#passResetForm').css('display', 'block');
+  }
+  $('#login').click(login);
+  $('#signUp').click(signUpForm);
+  $('#passwordReset').click(passResetSetUp);
+  //You can then get the user's basic profile information from the User object. 
+  function SignIn() {
+    if (firebase.auth().currentUser) {
+      // [START signout]
+      firebase.auth().signOut();
+      // [END signout]
+    } else {
+      var email = $('#email').val();
+      var password = $('#password').val();
+      if (email.length < 4) {
+        $('.alert').text('Please enter an email address.');
+        return;
+      }
+      if (password.length < 4) {
+        $('.alert').text('Please enter a password.');
+        return;
+      }
+  
+      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode === 'auth/wrong-password') {
+          $('.alert').text('Wrong password.');
+        } else {
+          $('.alert').text(errorMessage);
+        }
+        console.log(error);
+      });
+    }
+    $('.container').css('display','block');
+    $('#signInForm').css('display', 'none');
+  }
+  function SignUp() {
+    var email = $('#emailInput').val();
+    var password = $('#passwordInput').val();  
+    if (email.length < 4) {
+      $('.alert').text('Please enter a valid email address.');
+      return;
+    }
+    if (password.length < 4) {
+      $('.alert').text('Password must be at least 6 characters.');
+      return;
+    }
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorCode == 'auth/weak-password') {
+      $('.alert').text('The password is too weak.');
+    } else {
+      $('.alert').text(errorMessage);
+    }
+    console.log(error);
+    // [END_EXCLUDE]
+  });
+  // [END createwithemail]
+  $('.container').css('display', 'block');
+  $('.signUpForm').css('display', 'none');
+};
+
+function sendPasswordReset() {
+  var email = $('#emailInput').val();
+  // [START sendpasswordemail]
+  firebase.auth().sendPasswordResetEmail(email).then(function() {
+    // Password Reset Email Sent!
+    // [START_EXCLUDE]
+    $('.alert').text('Password Reset Email Sent!');
+    // [END_EXCLUDE]
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // [START_EXCLUDE]
+    if (errorCode == 'auth/invalid-email') {
+      $('.alert').text(errorMessage);
+    } else if (errorCode == 'auth/user-not-found') {
+      $('.alert').text(errorMessage);
+    }
+    console.log(error);
+    // [END_EXCLUDE]
+  });
+  // [END sendpasswordemail];
+}
+
+function initApp() {
+  // Listening for auth state changes.
+  // [START authstatelistener]
+  firebase.auth().onAuthStateChanged(function(user) {
+
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // [START_EXCLUDE]
+      $('#sign-out').css('display', 'block');
+      $('#sign-in').css('display', 'none');
+      $('#userName').text(displayName);
+
+      // [END_EXCLUDE]
+    } else {
+      // User is signed out.
+      // [START_EXCLUDE]
+      $('#sign-in').css('display', 'block');
+      $('#sign-out').css('display', 'none');
+      // [END_EXCLUDE]
+    }
+
+    $('#sign-in').click(SignIn);
+    $('#sign-up').click(SignUp);
+    $('#passwordReset').click(sendPasswordReset);
+    $('#sign-out').click(function(){
+      firebase.auth().signOut();
+    });
+  });
+  
+
   //Utelly for Netflix, google play and amazon prime
   var searchInput ="";
   //omdb variables
@@ -16,8 +151,7 @@
   var omdbType= "";
   var year= "";
   var omdbApiKey = "7cc4d503";
-
-
+  var gbApiKey= "1da49a76dc5aff961a13224d42119be60f600160";
  
 $(".search").click(function(e){
     e.preventDefault();
@@ -29,7 +163,13 @@ $(".search").click(function(e){
 
 
     var omdbQueryURL = "https://www.omdbapi.com/?s=" + searchInput + "&type=" + omdbType + "&y=" + year + "&plot=short&apikey=" + omdbApiKey;
-  
+    var gbQueryURL= "https://api-public.guidebox.com/v2/channels??api_key="+gbApiKey+"&type=movie&query="+searchInput;
+    $.ajax({
+      url: gbQueryURL,
+      method:"GET"
+    }).then(function(response){
+      console.log(response);
+    })
     $.ajax({
       url: utellyQueryURL,
       headers:{
@@ -44,10 +184,16 @@ $(".search").click(function(e){
         for(var x=0; x<locations.length; x++){
           var div= $("<div>");
           var img = $("<img>");
-          div.text(results[i].name);
-          div.append(results[i].locations[x].display_name);
+          var h2= $("<h2>");
+          var p= $("<p>");
+          div.css("myDiv");
+          h2.text(results[i].name);
+          p.append(results[i].locations[x].display_name);
           img.attr("src", results[i].picture);
-          div.append(img);
+          img.attr("height", "50%");
+          img.attr("width", "50%");
+          img.css("float-left");
+          div.append(h2, p,img);
           $("body").append(div);
         }
       }
@@ -58,19 +204,6 @@ $(".search").click(function(e){
         method: "GET"
       }).then(function(response) {
         console.log(response);
-      });
-})
-
-
-  //trakt ajax call
-  $.ajax({ 
-    url: "https://cors-anywhere.herokuapp.com/https://api.trakt.tv/calendars/all/movies/2014-09-01/7", 
-    headers: {
-      "Content-type": "application/json",
-      "trakt-api-key": "c012ee729c5660df87999860a3b52eaaf0ecd5d6e6c535f76341feafa05eb28d",
-      "trakt-api-version": "2"
-    },
-    method: "GET"})
-  .then(function(response){
-      console.log(response)
-  });
+      })
+    });
+  }
