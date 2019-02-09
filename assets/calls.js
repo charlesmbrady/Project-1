7052a692524f29a6f294fac57248579d7f77f851
Wes Hatley <weshatley@gmail.com>
2019-02-09 04:12:27 -0500

    fixed show search, added filter by free or paid, other cosmetic and ancillary changes

--- assets/calls.js
+++ assets/calls.js
@@ -2,38 +2,23 @@
 var omdbApiKey = "&apikey=7cc4d503";
 var gbApiKey = "?api_key=1da49a76dc5aff961a13224d42119be60f600160";
 var searchType = $('#search-type');
+var sourceType = $('#source-type');
 
-// Change search type on-click to be either by movie or show; default is movie:
-$('#search-type').on('click', function changeSearchType (event){
-  event.preventDefault();
-  if (searchType.hasClass('movies')){
-    searchType.removeClass('movies');
-    searchType.addClass('shows');
-    $('#search-type').text('Currently Searching By Shows');    
-  } else {
-    searchType.removeClass('shows');
-    searchType.addClass('movies');
-    $('#search-type').text('Currently Searching By Movies');
-  }
-})
+// On-Click search function for OMDB API call: 
 
-//Search-On-Click Function: 
 $("#search-button").click(function (event) {
   event.preventDefault();
   $("#results-display").css('display', 'block');
   $("#results-display").remove(".package");
   var searchInput = $("#search-input").val().trim();
-  var omdbQueryURL = 'https://www.omdbapi.com/?s=' + searchInput + omdbApiKey;
-  // OMDB API AJAX call:
+  var typeO; if (searchType.hasClass('movies')) {var typeO='&type=movie'} else {var typeO='&type=series'};
+  var omdbQueryURL = 'https://www.omdbapi.com/?s='+searchInput+typeO+omdbApiKey;
+  console.log("This is the passed omdbQueryURL: "+omdbQueryURL);
   $.get(omdbQueryURL)
     .done(function (omdbResponse) {
       $('#search-input').val('');
-
       var omdbResults = omdbResponse.Search;
-    
-      if (omdbResults == '') {
-        $("<div class='col-md-12'>").text("There isn't a result for this search. Womp womp.").appendTo('#results-display');
-        return 0;}
+      if (omdbResults == '') {$("<div class='col-md-12'>").text("There isn't a result for this search. Womp womp.").appendTo('#results-display'); return 0;}
       for (var i = 0; i < omdbResults.length; i++) {
         var imdbID = omdbResults[i].imdbID;
         var posterSrc = omdbResults[i].Poster;
@@ -47,14 +32,14 @@ $("#search-button").click(function (event) {
     });
 });
 
-//On-Click handler for calling additional details:
+// On-Click function for OMDB API calling additional media details:
+
 $(document).on("click", ".details", function () {
   $("#details").text("");
   var id = $(this).parent().attr("data-imdbID");
-  var omdbDetailsURL = "http://www.omdbapi.com/?i=" + id + "&plot=full" + omdbApiKey;
+  var omdbDetailsURL = "http://www.omdbapi.com/?i="+id+"&plot=full"+omdbApiKey;
   $.get(omdbDetailsURL)
     .done(function (response) {
-      console.log(response);
       var title = $("<p>").text(response.Title);
       var rating = $("<p>").text("Rated: " + response.Rated);
       var plot = $("<p>").text(response.Plot);
@@ -62,81 +47,60 @@ $(document).on("click", ".details", function () {
     });
 });
 
-// GuideBox API AJAX call:
+// On-Click function for GuideBox API call:
+
 $("#results-display").on("click", ".streamSearch", function () {
   var id = $(this).parent().attr("data-imdbID");
   fadeServiceIcons();
-  console.log("working");
-  console.log(id);
+  console.log("The GuideBox call is working if you get this message.");
+  console.log('-----------------------------------------------------')
+  console.log('This is the searched items IMDB ID: '+id);
+  console.log('-----------------------------------------------------')
   var guideboxId;
-  var showguideboxId;
-  var type; if (searchType.hasClass('movie')) {var type='movies'} else {var type='shows'};
-  //TESTING EXAMPLE: Show title: Arrow , IMDB id = tt2193021   GB id = 13015
-  //var gbQueryURL = "http://api-public.guidebox.com/v2/movies/13015/sources" + gbApiKey;
-
-  var gbQueryURLimdb = "http://api-public.guidebox.com/v2/search" + gbApiKey + "&type=movie&field=id&id_type=imdb&query=" +id;
-  //TODO: if the gbResponseID is undefined, it's because the clicked poster is actually related to a show instead of a movie, need to accomodate this...
-  
-  //first guidebox call to get the guidebox movie/show id (needed to get sources)
-   $.ajax({
-    url: gbQueryURLimdb,
-    method: 'GET'
-   }).then(function (gbResponse) {
-    console.log(gbResponse);
-    guideboxId = gbResponse.id;
-    console.log("heres the id " + guideboxId);
-   if (gbResponse.results == '') {
-    $("<div class='col-md-12'>").text("There isn't a result for this search. Womp womp. :(").appendTo('#results-display')
-    };
-
-    console.log(guideboxId)
-    console.log(gbApiKey)
-    var gbQueryURLfinal = "https://api-public.guidebox.com/v2/movies/"+guideboxId+gbApiKey+"&sources=subscription" ;
-    $.ajax({
-     url: gbQueryURLfinal,
-     method: 'GET'
-     }).then(function (gbResponse) {
-     console.log(gbResponse);
-     console.log(gbResponse.subscription_web_sources);
-     gbResponse.subscription_web_sources.forEach(checkSources);
-   });
-   });
-   
-   //for shows
-   var gbQueryURLimdb = "http://api-public.guidebox.com/v2/search" + gbApiKey + "&type=show&field=id&id_type=imdb&query=" +id;
-  
-  $.ajax({
-    url: gbQueryURLimdb,
-    method: 'GET'
-   }).then(function (gbResponse) {
-    console.log(gbResponse);
-   showguideboxId = gbResponse.id;
-   console.log("heres the id " + showguideboxId);
-  if (gbResponse.results == '') {
-    $("<div class='col-md-12'>").text("There isn't a result for this search. Womp womp. :(").appendTo('#results-display')
-    };
-
-     console.log(showguideboxId)
-     console.log(gbApiKey)
-    var gbQueryURLfinal = "https://api-public.guidebox.com/v2/shows/"+showguideboxId+"seasons"+gbApiKey+"&sources=subscription" ;
-     $.ajax({
-      url: gbQueryURLfinal,
-      method: 'GET'
-      }).then(function (gbResponse) {
+  var typeA; if (searchType.hasClass('movies')) {var typeA='movies/'} else {var typeA='shows/'};
+  var typeB; if (searchType.hasClass('movies')) {var typeB='&type=movie'} else {var typeB='&type=show'};
+  var costSource; if (sourceType.hasClass('free')) {var costSource="&sources=free"} else {var costSource="&sources=subscription"}
+  var gbQueryURLimdb = "http://api-public.guidebox.com/v2/search"+gbApiKey+typeB+"&field=id&id_type=imdb&query="+id;
+  console.log("This is the passed gbQueryURLimdb: "+gbQueryURLimdb);
+  console.log('-----------------------------------------------------')
+  // First GuideBox call to get the GuideBox ID (required to get streaming sources):
+  $.get(gbQueryURLimdb)
+    .done(function (gbResponse) {
       console.log(gbResponse);
+      console.log('-----------------------------------------------------')
+      guideboxId = gbResponse.id;
+      console.log("This is the searched items GuideBox ID: "+guideboxId);
+      console.log('-----------------------------------------------------')
+      if (gbResponse.results == '') {$("<div class='col-md-12'>").text("There isn't a result for this search. Womp womp. :(").appendTo('#results-display')};
+      var gbQueryURLfinal = "https://api-public.guidebox.com/v2/"+typeA+guideboxId+gbApiKey+costSource
+      console.log("This is the passed gbQueryURLfinal: "+gbQueryURLfinal);
+      console.log('-----------------------------------------------------')
+  // Second GuideBox call to map streaming sources available for searched media:
+      $.get(gbQueryURLfinal)
+          .done(function (gbResponse) {
+            $('.streamingservicecontainer').show();
+            console.log(gbResponse);
+            // Subscription services media is available on:
+            gbResponse.subscription_web_sources.forEach(checkSources);
+            gbResponse.subscription_ios_sources.forEach(checkSources);
+            gbResponse.subscription_android_sources.forEach(checkSources);
+            // Services media can be purchased on:
+            gbResponse.purchase_android_sources.forEach(checkSources);
+            gbResponse.purchase_ios_sources.forEach(checkSources);
+            gbResponse.purchase_web_sources.forEach(checkSources);
+            // Free services media is available on:
+            gbResponse.free_web_sources.forEach(checkSources);
+            gbResponse.free_ios_sources.forEach(checkSources);
+            gbResponse.free_android_sources.forEach(checkSources);
+            // TV Everywhere service availability:
+            gbResponse.tv_everywhere_android_sources.forEach(checkSources);
+            gbResponse.tv_everywhere_ios_sources.forEach(checkSources);
+            gbResponse.tv_everywhere_web_sources.forEach(checkSources);
+            });
     });
-    });
-  //second guidebox call to get the sources using the guidebox id
-
 });
-  //second guidebox call to get the sources using the guidebox id
 
-
-
-
-// Sets default text and search type for togglable button:
-$('#search-type').text('Currently Searching By Movies');
-
+// Function to render logo display based on source being available:
 
 function checkSources(src) {
   if(src.source.includes("netflix")) {
@@ -151,11 +115,21 @@ function checkSources(src) {
   if(src.source.includes("youtube")) {
     $("#youtube").css("opacity", "1");
   }
-  if(src.source.includes("amazon_prime")) {
+  if(src.source.includes("amazon")) {
     $("#primevideo").css("opacity", "1");
   }
-
+  if(src.source.includes("itunes")) {
+    $("#itunes").css("opacity", "1");
+  }
+  if(src.source.includes("vudu")) {
+    $("#vudu").css("opacity", "1");
+  }
+  if(src.source.includes("google")) {
+    $("#googleplay").css("opacity", "1");
+  }
 }
+
+// Function to render logo display based on source being unavailable:
 
 function fadeServiceIcons() {
   $("#netflix").css("opacity", ".5");
@@ -163,4 +137,7 @@ function fadeServiceIcons() {
   $("#hulu").css("opacity", ".5");
   $("#primevideo").css("opacity", ".5");
   $("#youtube").css("opacity", ".5");
+  $("#itunes").css("opacity", ".5");
+  $("#vudu").css("opacity", ".5");
+  $("#googleplay").css("opacity", ".5");
 }
